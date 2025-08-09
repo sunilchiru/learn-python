f5ad4a87a0cd52accda1c982960ce11998eda52c
sunilisari <sunilisari2114@gmail.com>
2025-08-09 16:08:44 +0530

    updated the privos code

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # Code to pring Simple Hello, world and Bye, world and 1 to 10
 
-print('Hello, world!')
-for i in range(1, 11):
-    print(i)
-print('Bye, world!')
+print('welcome to python coding!')
+for i in range(1,5):
+  print(f'Bye to the person id:{i}!')
 
 name = input('Enter your name: ')
 age = int(input(f'Enter your age: '))
-print(f'Hi, {name}!')
+print(f'Hi, {name}!, you are{age} years old')
